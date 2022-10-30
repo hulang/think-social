5941ca1ab49bf96b36254200b11e1aeb39094c43
快乐浪子哥 <hulangfy@163.com>
2022-10-30 14:26:29 +0800

    update

---
+++ src/config/config.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // 腾讯QQ|登录配置
+    'qq' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // 微信扫码|登录配置
+    'weixin' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // 新浪|登录配置
+    'sina' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Baidu|登录配置
+    'baidu' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Gitee|登录配置
+    'gitee' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Github|登录配置
+    'github' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Google|登录配置
+    'google' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Facebook|登录配置
+    'facebook' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Oschina|登录配置
+    'oschina' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Taobao|登录配置
+    'taobao' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Douyin|登录配置
+    'douyin' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Xiaomi|登录配置
+    'xiaomi' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ],
+    // Dingtalk|登录配置
+    'dingtalk' => [
+        'app_key' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'callback' => '', // 应用回调地址
+    ]
+];

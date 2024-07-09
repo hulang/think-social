cd6dd18740bcb43162f96b4a21c9c046d22601f5
快乐浪子哥 <hulangfy@163.com>
2024-07-09 08:48:02 +0800

    Update

--- src/config/config.php
+++ src/config/config.php
@@ -5,20 +5,20 @@ declare(strict_types=1);
 return [
     // 腾讯QQ登录配置
     'qq' => [
-        'app_id'        => '', //应用注册成功后分配的 APP ID
-        'app_secret'    => '',  //应用注册成功后分配的KEY
-        'scope'         => 'get_user_info',
+        'app_id' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'scope' => 'get_user_info',
     ],
     // 微信扫码登录配置
     'weixin' => [
-        'app_id'        => '', //应用注册成功后分配的 APP ID
-        'app_secret'    => '',  //应用注册成功后分配的KEY
-        'scope'         => 'snsapi_userinfo', // 如果需要静默授权,这里改成:snsapi_base,扫码登录系统会自动改为:snsapi_login
+        'app_id' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'scope' => 'snsapi_userinfo', // 如果需要静默授权,这里改成:snsapi_base,扫码登录系统会自动改为:snsapi_login
     ],
     // 新浪登录配置
     'sina' => [
-        'app_id'        => '', //应用注册成功后分配的 APP ID
-        'app_secret'    => '',  //应用注册成功后分配的KEY
-        'scope'         => 'all',
+        'app_id' => '', //应用注册成功后分配的 APP ID
+        'app_secret' => '',  //应用注册成功后分配的KEY
+        'scope' => 'all',
     ],
 ];
